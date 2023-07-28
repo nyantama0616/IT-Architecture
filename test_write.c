b3e86bdaab4535d37e6e9266932b91f925732290
nyantama0616 <r_nyantama0616@yahoo.co.jp>
2023-07-28 15:42:07 +0900

    writeをテストするcスクリプトを追加

---
+++ test_write.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include <ctype.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <fcntl.h>
+#include <unistd.h>
+#include <sys/file.h>
+// #include <string.h>
+
+int main (int argc, char* argv[]) {
+    int device_fd = open(argv[1], O_RDWR);
+	if (device_fd < 0) {
+		perror(argv[1]);
+	}
+
+    unsigned char led_state = (unsigned char)strtol(argv[2], NULL, 16);
+	int ret;
+
+    ret = write(device_fd, &led_state, 1);
+    if (ret != 1) {
+        perror("write");
+        return 0;
+    }
+}
